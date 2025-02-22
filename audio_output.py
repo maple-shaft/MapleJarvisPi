c09321c24834d67abea2a5a50a397c7819ed8a80
= <dustin.alan.briscoe@gmail.com>
2025-02-22 18:57:23 -0500

    Oops, normalizing twice is abnormal.

--- audio_output.py
+++ audio_output.py
@@ -1,12 +1,17 @@
 
 import pyaudio as pa
 import numpy as np
+import pydub.playback
 import scipy as sp
 import time
+import io
 import sounddevice as sd
 from threading import Thread, Event
 import queue as q
 from multiprocessing import Queue
+from pydub import AudioSegment
+from pydub.effects import speedup
+from pydub.playback import play
 
 class AudioOutput:
 
@@ -38,6 +43,13 @@ class AudioOutput:
         else:
             self.fetch_thread.start()
 
+    def _play(self, data : np.ndarray, sample_rate : int, speedup_flag : bool = True):
+        buffer = io.BytesIO(data.tobytes())
+        aus = AudioSegment.from_raw(buffer, sample_width=2, frame_rate=sample_rate, channels=1)
+        if speedup_flag:
+            aus = speedup(aus, 1.25)
+        play(aus)
+
     def _fetch_and_play(self):
         debug = self.debug
         print(f"AudioOutput: Starting fetch_and_play runnable {self.shutdown_event}") if debug else None
@@ -52,9 +64,8 @@ class AudioOutput:
                     print(f"AudioOutput.fetch_and_play: Next bytes fetched for playing: {type(data)}") if debug else None
                     print(f"AudioOutput.fetch_and_play: Devices: {sd.query_devices()}") if debug else None
                     
-                    # Maybe speed this up with pydub?
                     sd.play(data = data,
-                            samplerate = 40100,
+                            samplerate = 21000,
                             blocking = True
                     )
             except KeyboardInterrupt:
